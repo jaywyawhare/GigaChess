83d5cbf2adf5cd7fe4234db64ebfcf46a6ac86a9
jaywyawhare <2005441@kiit.ac.in>
2025-04-23 14:45:26 +0530

    added color and turn based support

--- main.py
+++ main.py
@@ -15,28 +15,37 @@ class ChessEngine:
         self.evaluator = Evaluator(self.piece_squares)
         self.search = MinimaxSearch(self.evaluator.evaluate)
         self.validator = MoveValidator()
+        self.current_color = chess.WHITE
 
     def get_valid_moves(self):
         return list(self.board.legal_moves)
 
     def make_move(self):
+        if self.board.turn != self.current_color:
+            self.current_color = not self.current_color
+            return None
+
         move = self.search.find_best_move(self.board)
         if move and self.validator.is_legal_move(self.board, move):
+            self.current_color = not self.current_color
             return move
         return None
 
 
 def main():
     engine = ChessEngine()
+    white_to_move = True
 
     while not engine.board.is_game_over():
-        print(engine.board)
-        print("\nCurrent evaluation:", engine.evaluator.evaluate(engine.board))
+        print("\n" + str(engine.board))
+        print(f"\n{'White' if white_to_move else 'Black'} to move")
+        print("Current evaluation:", engine.evaluator.evaluate(engine.board))
 
         move = engine.make_move()
         if move:
             engine.board.push(move)
             print(f"Move made: {move}")
+            white_to_move = not white_to_move
         else:
             break
 
